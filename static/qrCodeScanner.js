74713a849946b24eec3ab545759e553cfff1faea
Gabriela Acuña <54443293+gabiacuna@users.noreply.github.com>
2023-08-24 21:55:25 -0400

    video permisions

--- static/qrCodeScanner.js
+++ static/qrCodeScanner.js
@@ -45,26 +45,20 @@ my_qrcode.callback = async (res) => {
 };
 
 btnScanQR.onclick = () => {
-  document.body.style.background = "Black";
-  navigator.permissions.query({ name: "camera" }).then((result) => {
-    if (result.state === "granted") {
-      navigator.mediaDevices
-        .getUserMedia({ video: { facingMode: "environment" } })
-        .then(function (stream) {
-          scanning = true;
-          qrResult.hidden = true;
-          btnScanQR.hidden = true;
-          canvasElement.hidden = false;
-          video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-          video.srcObject = stream;
-          video.play();
-          tick();
-          scan();
-        });
-    } else if (result.state === "prompt") {
-      alert("Debe permitir el acceso a la cámara para poder escanear el código QR");
-    }
-  });
+  document.body.style.background = "Black";    
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function (stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
 }; 
 
 function tick() {
