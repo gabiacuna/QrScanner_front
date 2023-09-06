5e29af09dcfff769a40c11f6d792af39d2e4c753
gabiacuna <gabriela.acuna@usm.cl>
2023-09-06 19:46:31 -0300

    rid of form

--- static/manual.js
+++ static/manual.js
@@ -1,9 +1,12 @@
 const manualResult = document.getElementById("manual-result");
 const outputDataManual = document.getElementById("outputDataManual");
-const form = document.getElementById("manual_input");
 
-async function sendData() {
-    const data = new FormData(form);
+const btnSend = document.getElementById("btn-send-manual");
+const rut = document.getElementById("rut");
+
+btnSend.onclick = async () => {
+    const data = new FormData();
+    data.append("rut", rut.value);
     console.log(data.get("rut"));
     const response = await fetch("https://gabigabi.xyz:8000/validate", {method: "POST", body: data, redirect: 'follow'});
     const validate = await response;
@@ -20,8 +23,4 @@ async function sendData() {
     outputDataManual.innerText = "Error";
     document.body.style.background = "Black";
     }
-}
-
-if (form){
-    form.addEventListener("submit", () => sendData(), false);
 }
