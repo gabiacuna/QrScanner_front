1d189031db7493769e72b85b2bda8174990d22b7
gabiacuna <gabriela.acuna@usm.cl>
2023-09-06 20:10:51 -0300

    null fix

--- static/manual.js
+++ static/manual.js
@@ -1,30 +1,31 @@
-const manualResult = document.getElementById("manual-result");
-const outputDataManual = document.getElementById("outputDataManual");
-const form = document.getElementById("manual_input");
+document.addEventListener("DOMContentLoaded", function () {
+    const manualResult = document.getElementById("manual-result");
+    const outputDataManual = document.getElementById("outputDataManual");
+    const form = document.getElementById("manual_input");
 
-const btnSend = document.getElementById("btn-send-manual");
-const rut = document.getElementById("rut");
+    const btnSend = document.getElementById("btn-send-manual");
 
-btnSend.addEventListener("click", function () {
+    btnSend.addEventListener("click", function () {
 
-    const formData = new FormData(form);
+        const formData = new FormData(form);
 
-    fetch("https://gabigabi.xyz:8000/validate", {
-        method: "POST",
-        body: formData,
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse the response JSON if needed
-    })
-    .then(data => {
-        // Handle the response data here
-        console.log("Response data:", data);
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-        console.error("Fetch error:", error);
+        fetch("https://gabigabi.xyz:8000/validate", {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json(); // Parse the response JSON if needed
+        })
+        .then(data => {
+            // Handle the response data here
+            console.log("Response data:", data);
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch
+            console.error("Fetch error:", error);
+        });
     });
 });
